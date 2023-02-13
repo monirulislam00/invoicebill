1de03e39177450fa9f4da1cfc1b838c7b502271f
Nurul Komor <58949982+nurul-komor@users.noreply.github.com>
2023-02-14 00:02:02 +0600

    broken table fixed

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -13,18 +13,17 @@
         td {
             border: 1px solid black;
             border-collapse: collapse;
-            padding: 5px
+            padding: 3px
         }
     </style>
 
 </head>
 
 <body>
-
-    <table class="waffle no-grid table table-bordered text-center" cellpadding="0">
+    <table style="width:100%">
         <thead>
             <tr>
-                <th colspan="11" rowspan="2">
+                <th colspan="11">
                     <span style="font-size:22pt;color:#ff0000;">SIM</span><span style="font-size:22pt;">RAK</span>
                     ENTERPRISE<br />SUPPLYING
                     QUALITY PRODUCTS<br />CONTACT : 01866-519376
@@ -33,184 +32,167 @@
         </thead>
         <tbody>
             <tr style="height: 26px">
-                <td class="s2" colspan="2">BUYER NAME</td>
-                <td class="s3" colspan="6">MD JANE ALAM</td>
-                <td class="s5"></td>
-                <td class="s5"></td>
-                <td class="s5"></td>
+                <td colspan="2">BUYER NAME</td>
+                <td colspan="6">MD JANE ALAM</td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">SELLER NAME</td>
-                <td class="s6" colspan="6">AMRAN BAHADUR</td>
-                <td class="s5"></td>
-                <td class="s5"></td>
-                <td class="s5"></td>
+                <td colspan="2">SELLER NAME</td>
+                <td colspan="6">AMRAN BAHADUR</td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">SUPPLIER</td>
-                <td class="s6" colspan="6">MD SHAKIBUL HOSSAIN SIKDER</td>
-                <td class="s7" colspan="3" rowspan="6"></td>
+                <td colspan="2">SUPPLIER</td>
+                <td colspan="6">MD SHAKIBUL HOSSAIN SIKDER</td>
+                <td colspan="3" rowspan="6" style="min-width:150px"></td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">FACTORY</td>
-                <td class="s6" colspan="6">HARUALCHARI BRICKS</td>
+                <td colspan="2">FACTORY</td>
+                <td colspan="6">HARUALCHARI BRICKS</td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">TRADEMARK</td>
-                <td class="s6" colspan="6">HB</td>
+                <td colspan="2">TRADEMARK</td>
+                <td colspan="6">HB</td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">DATE</td>
-                <td class="s6" colspan="6">29.01.2023</td>
+                <td colspan="2">DATE</td>
+                <td colspan="6">29.01.2023</td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">LOCATION</td>
-                <td class="s6" colspan="6">BABY TOWER, REAZUDDIN BAZAR. CHATTOGRAM</td>
+                <td colspan="2">LOCATION</td>
+                <td colspan="6">BABY TOWER, REAZUDDIN BAZAR. CHATTOGRAM</td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">TYPE</td>
-                <td class="s6" colspan="6">1 NO</td>
+                <td colspan="2">TYPE</td>
+                <td colspan="6">1 NO</td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">PIECES</td>
-                <td class="s6" colspan="6">8000</td>
-                <td class="s8" colspan="3">ONE98HB04</td>
+                <td colspan="2">PIECES</td>
+                <td colspan="6">8000</td>
+                <td colspan="3">ONE98HB04</td>
             </tr>
             <tr style="height: 26px">
-                <td class="s9" colspan="11"></td>
+                <td colspan="11"></td>
             </tr>
             <tr style="height: 26px">
-                <td class="s10"></td>
-                <td class="s11" colspan="9">PRICING</td>
-                <td class="s12"></td>
+                <td></td>
+                <td colspan="9">PRICING</td>
+                <td></td>
             </tr>
             <tr style="height: 26px">
-                <td class="s10"></td>
-                <td class="s13">NO</td>
-                <td class="s13" colspan="2">PIECES</td>
-                <td class="s13" colspan="2">UNIT PRICE</td>
-                <td class="s13" colspan="2">TOTAL PRICE</td>
-                <td class="s10"></td>
-                <td class="s10"></td>
-                <td class="s12"></td>
+                <td></td>
+                <td>NO</td>
+                <td colspan="2">PIECES</td>
+                <td colspan="2">UNIT PRICE</td>
+                <td colspan="2">TOTAL PRICE</td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
             <tr style="height: 26px">
-                <td class="s10"></td>
-                <td class="s14">1</td>
-                <td class="s15" colspan="2">8000</td>
-                <td class="s15" colspan="2">9.8</td>
-                <td class="s15" colspan="2">78400</td>
-                <td class="s12"></td>
-                <td class="s12"></td>
-                <td class="s12"></td>
+                <td></td>
+                <td>1</td>
+                <td colspan="2">8000</td>
+                <td colspan="2">9.8</td>
+                <td colspan="2">78400</td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
             <tr style="height: 26px">
-                <td class="s10"></td>
-                <td class="s14">2</td>
-                <td class="s12" colspan="2"></td>
-                <td class="s12" colspan="2"></td>
-                <td class="s15" colspan="2">0</td>
-                <td class="s16" colspan="3" rowspan="2">78400</td>
+                <td></td>
+                <td>2</td>
+                <td colspan="2"></td>
+                <td colspan="2"></td>
+                <td colspan="2">0</td>
+                <td colspan="3" rowspan="2">78400</td>
             </tr>
             <tr style="height: 26px">
-                <td class="s10"></td>
-                <td class="s14">3</td>
-                <td class="s12" colspan="2"></td>
-                <td class="s12" colspan="2"></td>
-                <td class="s15" colspan="2">0</td>
+                <td></td>
+                <td>3</td>
+                <td colspan="2"></td>
+                <td colspan="2"></td>
+                <td colspan="2">0</td>
             </tr>
             <tr style="height: 26px">
-                <td class="s10"></td>
-                <td class="s14">4</td>
-                <td class="s12" colspan="2"></td>
-                <td class="s12" colspan="2"></td>
-                <td class="s15" colspan="2">0</td>
-                <td class="s12"></td>
-                <td class="s12"></td>
-                <td class="s12"></td>
+                <td></td>
+                <td>4</td>
+                <td colspan="2"></td>
+                <td colspan="2"></td>
+                <td colspan="2">0</td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
             <tr style="height: 26px">
-                <td class="s17" colspan="11"></td>
+                <td colspan="11"></td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">BUYER NAME</td>
-                <td class="s6" colspan="6">MD JANE ALAM</td>
-                <td class="s5"></td>
-                <td class="s5"></td>
-                <td class="s5"></td>
+                <td colspan="2">BUYER NAME</td>
+                <td colspan="6">MD JANE ALAM</td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">SELLER NAME</td>
-                <td class="s6" colspan="6">AMRAN BAHADUR</td>
-                <td class="s5"></td>
-                <td class="s5"></td>
-                <td class="s5"></td>
+                <td colspan="2">SELLER NAME</td>
+                <td colspan="6">AMRAN BAHADUR</td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">SUPPLIER</td>
-                <td class="s6" colspan="6">MD SHAKIBUL HOSSAIN SIKDER</td>
-                <td class="s17" colspan="3" rowspan="6"></td>
+                <td colspan="2">SUPPLIER</td>
+                <td colspan="6">MD SHAKIBUL HOSSAIN SIKDER</td>
+                <td colspan="3" rowspan="6" style="min-width:150px"></td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">FACTORY</td>
-                <td class="s6" colspan="6">HARUALCHARI BRICKS</td>
+                <td colspan="2">FACTORY</td>
+                <td colspan="6">HARUALCHARI BRICKS</td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">TRADEMARK</td>
-                <td class="s6" colspan="6">HB</td>
+                <td colspan="2">TRADEMARK</td>
+                <td colspan="6">HB</td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">DATE</td>
-                <td class="s6" colspan="6">29.01.2023</td>
+                <td colspan="2">DATE</td>
+                <td colspan="6">29.01.2023</td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">LOCATION</td>
-                <td class="s6" colspan="6">BABY TOWER, REAZUDDIN BAZAR. CHATTOGRAM</td>
+                <td colspan="2">LOCATION</td>
+                <td colspan="6">BABY TOWER, REAZUDDIN BAZAR. CHATTOGRAM</td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">TYPE</td>
-                <td class="s6" colspan="6">1 NO</td>
+                <td colspan="2">TYPE</td>
+                <td colspan="6">1 NO</td>
             </tr>
             <tr style="height: 26px">
-                <td class="s5" colspan="2">PIECES</td>
-                <td class="s6" colspan="6">8000</td>
-                <td class="s8" colspan="3">ONE98HB04</td>
+                <td colspan="2">PIECES</td>
+                <td colspan="6">8000</td>
+                <td colspan="3">ONE98HB04</td>
             </tr>
             <tr style="height: 21px">
-                <td class="s5"></td>
-                <td class="s5"></td>
-                <td class="s5"></td>
-                <td class="s5"></td>
-                <td class="s5"></td>
-                <td class="s5"></td>
-                <td class="s5"></td>
-                <td class="s5"></td>
-                <td class="s5"></td>
-                <td class="s5"></td>
-                <td class="s5"></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
             <tr style="height: 15px">
-                <td class="s18" colspan="11" rowspan="10"></td>
+                <td colspan="11" style="height: 60px"></td>
             </tr>
-            {{-- <tr style="height: 21px"></tr>
-            <tr style="height: 21px"></tr>
-            <tr style="height: 21px"></tr>
-            <tr style="height: 21px"></tr>
-            <tr style="height: 21px"></tr>
-            <tr style="height: 21px"></tr>
-            <tr style="height: 21px"></tr>
-            <tr style="height: 21px"></tr>
-            <tr style="height: 21px"></tr> --}}
         </tbody>
     </table>
-
-    <!-- Bootstap JavaScript and Popper.js -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
-        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
-    </script>
 </body>
 
 </html>
