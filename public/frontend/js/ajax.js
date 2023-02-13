5a04a62361e3772e8654e325947b88cbd81a665f
Nurul Komor <58949982+nurul-komor@users.noreply.github.com>
2023-02-13 19:53:53 +0600

    toast added

--- public/frontend/js/ajax.js
+++ public/frontend/js/ajax.js
@@ -14,10 +14,34 @@ $(document).ready(function () {
             processData: false,
             contentType: false,
             success: function (response, data) {
-                console.log(response);
-                console.log(data);
-                alert("created new invoice");
-                $("#watch-pdf").attr("href", `/pdf/view/${response.data.id}`);
+                console.log(data, ":", response.responseCode);
+                if (response.responseCode == 200) {
+                    $.toast({
+                        heading: "Success",
+                        text: "created new invoice",
+                        showHideTransition: "slide",
+                        icon: "success",
+                    });
+                    $("#watch-pdf").attr(
+                        "href",
+                        `/pdf/view/${response.data.id}`
+                    );
+                } else if (response.responseCode == 400) {
+                    $.each(response.data, function (index, warning) {
+                        console.log(warning);
+                        $.toast({
+                            heading: "Warning",
+                            text: warning,
+                            icon: "warning",
+                        });
+                    });
+                } else if (response.responseCode === 500) {
+                    $.toast({
+                        heading: "Error",
+                        text: "Internal Server Error",
+                        icon: "error",
+                    });
+                }
             },
             error: function (request, error) {
                 console.log(error);
