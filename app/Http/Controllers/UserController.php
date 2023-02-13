e2d84e0dc912b10ebf42aff710fdbb6e978b2f06
Nurul Komor <58949982+nurul-komor@users.noreply.github.com>
2023-02-13 20:39:08 +0600

    bug fixes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,10 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function UserDashboard(){
-        return view('user/user_dashboard');
+    public function UserDashboard()
+    {
+        return view('user.user_dashboard');
     }
-    public function AddUser(){
-        return view('admin/add_user');
+    public function AddUser()
+    {
+        return view('admin.add_user');
     }
 }
